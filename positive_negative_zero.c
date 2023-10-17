230851d18b39e6bfa7d8ff6b5df25b34e9a3efd4
cjcreator <103061808+cjcreator@users.noreply.github.com>
2023-10-18 01:52:13 +0530

    Create positive_negative_zero.c

---
+++ positive_negative_zero.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<math.h>
+int main() {
+    //wap to find positive,negative,zero
+    int n;
+    printf("enter a no:");
+    scanf("%d",&n);
+    if(n>0)printf("+ive");
+    else if(n<0)printf("-ve");
+    else printf("zero");
+    
+    
+    return 0;
+}
