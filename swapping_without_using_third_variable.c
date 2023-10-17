443551679ebdc12535add3bcc24dec7bf22856b9
cjcreator <103061808+cjcreator@users.noreply.github.com>
2023-10-17 06:37:44 -0700

    Update swapping_without_using_third_variable.c

--- swapping_without_using_third_variable.c
+++ swapping_without_using_third_variable.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+int main(){
+//WAP to swap two no without using third variable
+int a=10,b=20;
 printf("before swapping \n A= %d and B= %d",a,b);
+a=a^b;
+b=a^b;
+a=a^b;
+printf("\nafter swapping without using third variable!! \n A= %d and B= %d",a,b);
+return 0;
+}
