1afcfef7f25a875266bde768de069e3d3245751d
cjcreator <103061808+cjcreator@users.noreply.github.com>
2023-10-17 20:27:36 +0530

    Update electricity_bill.c

--- electricity_bill.c
+++ electricity_bill.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 int main(){
 /*
-Write a C program to calculate area of circle
+Write a C program to calculate bill according to following:
+Uttrakhand state electricity board bill is generated on the following basis:
+If units are less than or equal to 1000, rate is 5 rs/unit
+If units are greater than 1000 and less than or equal to 2000, rate is 6 rs/unit
+If units are greater than 2000, rate is 7rs/unit
 */
 int unit,rate,bill;
 
